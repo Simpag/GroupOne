b2d3ef98d9954ae2cf19a917e08ba9920e86dd21
Simon Granström <simon.granstrom@gmail.com>
2018-09-22 19:29:38 +0200

    Added a basic enemy and movement system

---
+++ No Homework!/Assets/Scripts/Singleplayer/Waypoints.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+    public static Transform[] waypoints;
+
+    private void Awake()
+    {
+        waypoints = new Transform[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++) //Save all of the waypoints in an array for the enemies to find
+        {
+            waypoints[i] = transform.GetChild(i);
+        }
+    }
+
+
+
+}
