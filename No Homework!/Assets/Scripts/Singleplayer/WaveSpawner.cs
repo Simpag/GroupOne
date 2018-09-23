75a4956edb8c934e40204dc132eddc932020bffa
Simon Granström <simon.granstrom@gmail.com>
2018-09-23 02:50:35 +0200

    Made a simple wave spawner

---
+++ No Homework!/Assets/Scripts/Singleplayer/WaveSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private Transform enemyPrefab;
+    [SerializeField]
+    private Transform spawnPoint;
+
+    [SerializeField]
+    private float waveTimeDelay = 5f;
+    [SerializeField]
+    private float spawnTimeDelay = 0.5f;
+    private float countDown = 2f;
+
+    private int waveIndex = 0;
+
+    private void Update()
+    {
+        if (countDown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = waveTimeDelay;
+        }
+
+        countDown -= Time.deltaTime;
+    }
+
+    private IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemie();
+            yield return new WaitForSeconds(spawnTimeDelay);
+        }
+
+        Debug.Log("Wave: " + waveIndex);
+    }
+
+    private void SpawnEnemie()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
