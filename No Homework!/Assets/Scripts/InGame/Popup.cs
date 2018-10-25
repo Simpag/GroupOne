d4f53156f8238aef88a44588268c7a51e803db98
Simon Granström <simon.granstrom@gmail.com>
2018-10-25 19:49:10 +0200

    Popup Messages

---
+++ No Homework!/Assets/Scripts/InGame/Popup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Popup : MonoBehaviour {
+
+    public Text text;
+
+	public void Setup(string _text)
+    {
+        CancelInvoke();
+        text.text = _text;
+        Invoke("Close", 40);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ No Homework!/Assets/Scripts/InGame/PopupManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupManager : MonoBehaviour {
+
+    private static PopupManager instance;
+    public static PopupManager Instance
+    {
+        get { return instance; }
+        set { instance = value; }
+    }
+
+    [SerializeField]
+    private string[] popuptext;
+    [SerializeField]
+    private GameObject popup;
+
+    private int index = 0;
+
+    private void Awake()
+    {
+        //Create singleton
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        popup.SetActive(false);
+
+        Invoke("ShowNextPopup", 5);
+        Invoke("ShowNextPopup", 20);
+        Invoke("ShowNextPopup", 50);
+    }
+
+    public void ShowNextPopup()
+    {
+        if (Instance.index >= Instance.popuptext.Length)
+            return;
+
+        Instance.popup.SetActive(true);
+        Instance.popup.GetComponent<Popup>().Setup(Instance.popuptext[Instance.index]);
+        Instance.index++;
+    }
+
+}
